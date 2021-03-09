32be720a9a2014006ddb7aeb453cb1fa35a8606c
Mauro <mauro.filho@ua.pt>
2021-03-09 10:36:29 +0000

    Initial Commit

---
+++ SimpleUnitTest/SimpleUnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleUnitTestApp.swift
+//  SimpleUnitTest
+//
+//  Created by Mauro Canhao on 09/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleUnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
